2a2361901183e21b49c853c482cb312eee021ec4
CuongstudyC <113528964+CuongstudyC@users.noreply.github.com>
2024-03-08 02:56:54 +0700

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createPool({
-    'host': 'localhost',
-    'database': 'demo',
-    'user': 'root',
-    'password': ''
+    'host': 'bde2uqaewvgvqvxnvs0m-mysql.services.clever-cloud.com',
+    'database': 'bde2uqaewvgvqvxnvs0m',
+    'user': 'uxuysonnovoj6ofh',
+    'password': 'LZFQ0jzB0UVxNEoRCSVf'
 })
 
 module.exports = connection;
