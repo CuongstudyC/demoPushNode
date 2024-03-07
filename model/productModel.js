0da92c2aaf2eca9a528ed941acd02031f65a4e62
CuongstudyC <113528964+CuongstudyC@users.noreply.github.com>
2024-03-08 02:44:38 +0700

    Update productModel.js

--- model/productModel.js
+++ model/productModel.js
@@ -1,10 +1,1 @@
-const mongoose = require('mongoose');
 
-const schema = mongoose.Schema;
-
-const products = new schema({
-    name: String,
-    price: Number
-})
-
-module.exports = mongoose.model('products', products);
